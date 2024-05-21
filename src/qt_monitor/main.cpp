8c467ff35c4a025ad57734d19c2aaaff755b5eed
XenoWYC121 <1299288319@qq.com>
2024-05-21 14:06:33 +0800

    ADD: 添加Qt模块

---
+++ src/qt_monitor/main.cpp
@@ -0,0 +1,11 @@
+#include "qt_monitor/mainwindow.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
